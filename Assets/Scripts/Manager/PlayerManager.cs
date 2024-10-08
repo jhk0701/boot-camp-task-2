c410b8b1ffa13cdc4814c2451c649eca8b0ee987
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-10-08 20:23:57 +0900

    Feat : Add player name input

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum CharacterType 
 {
@@ -12,18 +14,30 @@ public class PlayerManager : MonoBehaviour
     public Transform Player { get; private set; }
     GameObject currentRenderer;
     
+    [Header("Character Info")]
     // TODO : 캐릭터 관련 정보 분리하기
-    // 추후 변경 가능하도록
     public CharacterType currentCharacterType;
-    public List<GameObject> characters;
+    public GameObject[] characters;
+
+    [Header("Player Info")]
+    [SerializeField] Transform playerCanvas;
+    [SerializeField] TextMeshProUGUI txtPlayerName;
+    string playerName;
+
+    // TODO : 입력만 받는 클래스로 책임 분리
+    [Header("Player Name Input")]
+    [SerializeField] GameObject panelPlayerName;
 
     void Start()
     {
+        OpenPlayerNameInput();
+
         Player = Instantiate(prefPlayer).transform;
 
         CreatePlayerCharacter();
         
         Camera.main.transform.SetParent(Player);
+        playerCanvas.SetParent(Player);
     }
 
     public void CreatePlayerCharacter(CharacterType type = CharacterType.Penguin)
@@ -33,5 +47,27 @@ public class PlayerManager : MonoBehaviour
 
         AnimationController pAnim = Player.GetComponent<AnimationController>();
         pAnim.AssignAnimator(currentRenderer.GetComponent<Animator>());
+    }
+
+
+    public void OpenPlayerNameInput()
+    {
+        panelPlayerName.SetActive(true);
+    }
+
+    public void OnValueChangedPlayerName(string name)
+    {
+        // TODO : 정규식 적용
+        playerName = name;
+    }
+
+    public void CheckPlayerName()
+    {
+        if(playerName.Length == 0)
+            return;
+
+        panelPlayerName.SetActive(false);
+
+        txtPlayerName.text = playerName;
     }
 }
